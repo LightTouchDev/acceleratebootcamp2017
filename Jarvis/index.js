c7ce69181718c15319607495822048630ecff3cf
Aaron Lightfoot <aalight91@gmail.com>
2017-11-29 01:22:55 -0500

    Marvel project

--- Jarvis/index.js
+++ Jarvis/index.js
@@ -3,12 +3,9 @@
 var api = require('marvel-api') 
  
 var marvel = api.createClient({
-  publicKey: 'my-public-key'
-, privateKey: 'my-private-key'
+  publicKey: '51969ddf181dc7ee2d3b8b367c91dccf'
+, privateKey: '7139be6acf3ecf36fee0e7260489926739ed9046'
 })
- 
-//Having an issue creating/logging into account on marvel dev site
-//Will retry
 
 marvel.characters.findAll((err, results) => {
   if (err) {
