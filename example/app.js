969b7d0240597f0fd96375b53709055845ae166c
Aaron Lightfoot <aalight91@gmail.com>
2017-12-02 12:36:05 -0500

    Day 5 assignment

--- example/app.js
+++ example/app.js
@@ -9,7 +9,7 @@ app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname+'/dom.html'))
 
 })
-
+app.use('/', express.static(path.join(__dirname+'/')))
 
 app.listen('3000', () =>{
   console.log('Express listening on port 3000')
