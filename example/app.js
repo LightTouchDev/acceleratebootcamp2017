32c0817b0fe40e8092b74c6a41e297ca6f1f27b7
Aaron Lightfoot <aalight91@gmail.com>
2017-12-03 00:15:08 -0500

    Update 12.2

--- example/app.js
+++ example/app.js
@@ -3,13 +3,13 @@ let app = express()
 let path = require('path')
 //send .html to server
 
-
 app.get('/', (request, response) => {
   
   response.sendFile(path.join(__dirname+'/dom.html'))
 
 })
 app.use('/', express.static(path.join(__dirname+'/')))
+
 
 app.listen('3000', () =>{
   console.log('Express listening on port 3000')
--- example/domManip.js
+++ example/domManip.js
@@ -1,12 +1,27 @@
 //Create h1 tag
 
+let p1 = () => {
+	paragraph.style.textAlign = 'left'
+	paragraph.style.color = 'white'
+}
+
+document.body.style.backgroundColor = 'rgb(117, 68, 117)'
+
 let paragraph = document.createElement('p')
 paragraph.innerHTML = 'It all began with a bootcamp by Accelerate Foundation...'
+p1()
 
 let heading = document.createElement('h1')
-heading.innerHTML = 'This Is the Story of How I Became the Greatest Developer in the world'
+heading.innerHTML = 'This Is the Story of How I Became the Greatest Developer in the World'
+heading.style.textAlign = 'center'
+heading.style.color = 'rgb(9, 191, 11)'
+
+let button = document.createElement('button')
+button.innerHTML = 'click me!!'
+//doesn't display date when clicked. why?
+button.onClick = Date()
+
 
 let body = document.getElementById('content')
-body.append(heading, paragraph)
-
+body.append(heading, paragraph, button)
 
