76ad235873f986503ce403024403290fc27da004
Aaron Lightfoot <aalight91@gmail.com>
2017-12-04 10:46:23 -0500

    Day 4 assignment

--- jarvis-web-light/webpack.config.js
+++ jarvis-web-light/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
  module.exports = {
      entry: './app.js',
      output: {
-         path: path.resolve(__dirname, 'jarvis-web-light'),
+         path: path.resolve(__dirname, 'js'),
          filename: 'bundle.js'
      }
  };
